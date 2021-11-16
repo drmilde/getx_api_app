0bfeb41b1bc20a62b02650272526bed97d1fd3d0
drmilde <milde@hs-fulda.de>
2021-11-16 11:28:08 +0100

    GoogleFonts eingebaut

--- lib/screens/startup_screen.dart
+++ lib/screens/startup_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:getx_api_app/screens/home_screen.dart';
 import 'package:getx_api_app/screens/loader_screen.dart';
+import 'package:getx_api_app/widgets/text_widget.dart';
 
 class StartupScreen extends StatelessWidget {
   const StartupScreen({Key? key}) : super(key: key);
@@ -12,15 +12,37 @@ class StartupScreen extends StatelessWidget {
         title: Text("StartupScreen"),
       ),
       body: Center(
-        child: Container(
-          child: ElevatedButton(
-            child: Text("weiter"),
-            onPressed: () {
-              Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (context) => LoaderScreen()));
-            },
-          ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _space100(),
+            Expanded(
+                child: Center(
+              child: TextWidget(
+                  "Der StartScreen stellt den Einstieg in die App dar. "),
+            )),
+            _weiter(context),
+            _space100(),
+          ],
         ),
+      ),
+    );
+  }
+
+  Widget _space100() {
+    return SizedBox(
+      height: 100,
+    );
+  }
+
+  Widget _weiter(BuildContext context) {
+    return Container(
+      child: ElevatedButton(
+        child: Text("weiter"),
+        onPressed: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => LoaderScreen()));
+        },
       ),
     );
   }
