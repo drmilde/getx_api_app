43961cddae89d0e810cec88c1088d5d641612235
drmilde <milde@hs-fulda.de>
2021-12-13 15:28:24 +0100

    MaterialScrolBehavior eingebaut Im Web kann jetzt auch mit dem MousePointer gescrollt werden.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx_api_app/screens/themes/theme/theme_constants.dart';
@@ -7,6 +9,16 @@ import 'screens/startup_screen.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyCustomScrollBehavior extends MaterialScrollBehavior {
+  // Override behavior methods and getters like dragDevices
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+    PointerDeviceKind.touch,
+    PointerDeviceKind.mouse,
+    // etc.
+  };
 }
 
 // Hier wird der zentrale ThemeController erzeugt
@@ -20,6 +32,7 @@ class MyApp extends StatelessWidget {
     return Obx(() {
       return MaterialApp(
         debugShowCheckedModeBanner: false,
+        scrollBehavior: MyCustomScrollBehavior(),
         title: 'GetX-API',
         theme: themeController.currentThemeData,
         home: StartupScreen(),
